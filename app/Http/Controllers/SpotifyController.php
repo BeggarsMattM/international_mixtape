29ce01bdfb84bf556c71f8a06ce0707ee20446d1
Matthew Marcus <matthewmarcus@beggars.com>
2017-09-25 14:39:31 +0100

    Initial commit

---
+++ app/Http/Controllers/SpotifyController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use SpotifyWebAPI;
+
+class SpotifyController extends Controller
+{
+
+    public function __construct()
+    {
+      $this->session = new SpotifyWebAPI\Session(
+        env('CLIENT_ID'),
+        env('CLIENT_SECRET'),
+        env('REDIRECT_URI')
+      );
+
+      $this->api = new SpotifyWebAPI\SpotifyWebAPI();
+    }
+
+    public function connect()
+    {
+      $scopes = [];
+
+      $authorizeUrl = $this->session->getAuthorizeUrl(
+        ['scope' => $scopes, 'show_dialog' => true]
+      );
+
+      return redirect($authorizeUrl);
+    }
+
+    public function code(Request $request)
+    {
+      $code = $request->code;
+
+      $this->session->requestAccessToken($code);
+      $accessToken = $this->session->getAccessToken();
+
+      $this->api->setAccessToken($accessToken);
+
+      session(['refreshToken' => $this->session->getRefreshToken()]);
+
+      return view('mixtape')->with('access_token', $accessToken);
+
+    }
+
+}
