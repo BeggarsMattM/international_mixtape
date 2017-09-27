dbf1c0f53b5b6dd608f8b9e21baa9ba967e27f7f
Matthew Marcus <matthewmarcus@beggars.com>
2017-09-27 11:25:01 +0100

    Region select

--- resources/views/mixtape.blade.php
+++ resources/views/mixtape.blade.php
@@ -3,3 +3,6 @@
 <li>{{ $playlist->name }} ({{ $playlist->id }})</li>
 @endforeach
 </ul>
+<select class="crs-country" data-region-id="ABC"></select>
+<select id="ABC"></select>
+<script src="js/crs.min.js"></script>
